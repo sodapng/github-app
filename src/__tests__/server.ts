008d9110cb5e2ceef98cabf1e8eef894c98ef806
Kamran <sodapng@ya.ru>
2023-04-09 18:01:41 +0400

    chore: remove comment

--- src/__tests__/server.ts
+++ src/__tests__/server.ts
@@ -2,5 +2,4 @@ import { setupServer } from 'msw/node'
 
 import { handlers } from './handlers'
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 export const server = setupServer(...handlers)
