a21d70c5ec7413d5a5a55230f1a0519b094e143f
Kamran <sodapng@ya.ru>
2023-04-10 15:06:27 +0400

    styles: fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,23 +8,25 @@ type CardProperties = {
 
 export function Card({ image, name, onClick }: CardProperties) {
   return (
-    <div
-      data-testid={name}
-      onClick={onClick}
-      className='flex cursor-pointer flex-col gap-2 justify-self-center rounded-lg border p-2 shadow-md shadow-violet-100 hover:border-violet-500 hover:ring hover:ring-violet-300 hover:ring-offset-1'
-    >
-      <div className='h-36 overflow-hidden rounded-md'>
-        <img
-          aria-label='image'
-          loading='lazy'
-          className='object-cover'
-          src={image}
-          alt={image}
-          width={292}
-          height={144}
-        />
+    <div className='flex place-items-center'>
+      <div
+        data-testid={name}
+        onClick={onClick}
+        className='flex cursor-pointer flex-col gap-2 rounded-lg border p-2 shadow-md shadow-violet-100 hover:border-violet-500 hover:ring hover:ring-violet-300 hover:ring-offset-1'
+      >
+        <div className='h-36 overflow-hidden rounded-md'>
+          <img
+            aria-label='image'
+            loading='lazy'
+            className='object-cover'
+            src={image}
+            alt={image}
+            width={292}
+            height={144}
+          />
+        </div>
+        <h5 className='line-clamp-1 font-semibold'>{name}</h5>
       </div>
-      <h5 className='line-clamp-1 font-semibold'>{name}</h5>
     </div>
   )
 }
